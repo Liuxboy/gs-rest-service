e5f3f785cd0c229ad1244cbe262352e6ccd626a9
Liuchundong <liuchundong2008@gmail.com>
2016-12-19 10:41:46 +0800

    modify my spring boot example

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,6 +1,10 @@
 package hello;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +15,16 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/getGreeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+
+    @RequestMapping("/postGreeting")
+    public Greeting testPost(@RequestBody String str) {
+        Map map = JsonParserFactory.getJsonParser().parseMap(str);
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, map.get("name")));
     }
 }
